f91655faf44eca7879ef82c6ac2624516fadf140
pp0477285-hash <pp0477285@gmail.com>
2026-07-05 18:24:04 +0200

    Delete dashboard directory

--- dashboard/static/app.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".button");
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      button.classList.add("is-active");
-    });
-  });
-});
